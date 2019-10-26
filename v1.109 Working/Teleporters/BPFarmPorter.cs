20d7a098cf697a4b91d6401e0c0f76f6e268aa0c
Andrew Kirk <scrubtasticx@gmail.com>
2019-10-26 12:48:21 -0400

    Fixed a bunch more scripts for both versions

---
+++ v1.109 Working/Teleporters/BPFarmPorter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+using DOL.Events;
+using DOL.Database;
+using DOL.Database.Attributes;
+using DOL.AI.Brain;
+using DOL.GS.SkillHandler;
+using DOL.GS;
+using DOL.GS.Scripts;
+using DOL.GS.PacketHandler;
+using DOL.GS.Spells;
+using DOL.GS.Effects;
+
+using log4net;
+
+namespace DOL.GS.Scripts
+{
+    public class BPFarmPorter : GameNPC
+    {
+
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public override bool AddToWorld()
+        {
+            Name = "BP Farm";
+			GuildName = "Teleporter";
+            Model = 881;
+            Size = 37;
+            Level = 50;
+            Flags |= eFlags.PEACE;
+			
+			return base.AddToWorld();
+        }
+        public override bool Interact(GamePlayer player)
+        {
+            if (!base.Interact(player)) return false;
+            TurnTo(player.X, player.Y);
+            player.Out.SendMessage("Hello " + player.Name + ", You can currently be translocated to your [BPFarm Zone].  Number of Players Currently In your BPFarm Zone = " + WorldMgr.GetClientsOfRegionCount(249) + " ", eChatType.CT_Say, eChatLoc.CL_PopupWindow);
+            player.Bind(true);
+
+            return true;
+        }
+
+
+
+        public override bool WhisperReceive(GameLiving source, string str)
+        {
+            if (!base.WhisperReceive(source, str)) return false;
+            if (!(source is GamePlayer)) return false;
+            GamePlayer t = (GamePlayer)source;
+            TurnTo(t.X, t.Y);
+
+            switch (str)
+            {
+
+
+
+                #region BPFarm Zone
+
+                case "BPFarm Zone":
+				
+					if (!t.InCombat)
+                    {
+                    SendReply(t, "I'm now translocating you to the BPFarm zone!");
+                    t.MoveTo(249, 47260, 49577, 20831, 35);
+					}
+					else { t.Client.Out.SendMessage("You can't port while in combat.", eChatType.CT_Say, eChatLoc.CL_PopupWindow); }
+
+                    break;
+                #endregion BPFarm Zone
+
+                default: break;
+            }
+            return true;
+
+
+
+
+
+        }
+        private void SendReply(GamePlayer target, string msg)
+        {
+            target.Client.Out.SendMessage(
+                msg,
+                eChatType.CT_Say, eChatLoc.CL_PopupWindow);
+        }
+        [ScriptLoadedEvent]
+        public static void OnScriptCompiled(DOLEvent e, object sender, EventArgs args)
+        {
+            log.Info("\tTeleporter initialized: true");
+        }
+    }
+
+}
